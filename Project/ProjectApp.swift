68d269383a0ff1b4919d8ff908a798f50d05b2b5
NastyaKazieva <nastya.kazieva@gmail.com>
2025-07-17 12:36:18 +0300

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Nastya Shlepakova on 17/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
